c96638fc0fbb47789829b10fca51652edf2e4d96
Rodolfo Bandeira <rodolfo_band@hotmail.com>
2019-09-18 16:18:42 -0300

    Implementa requisiçoes de controle

--- web_server/web_server.ino
+++ web_server/web_server.ino
@@ -7,9 +7,9 @@ WiFiServer server(80);
 
 String HTTP_req;
 
-const byte qtdePinosES = 6;
-const byte pinosES[qtdePinosES]   =	{ 0     , 2     , 4 	 };
-const byte modoPinos[qtdePinosES] =	{ OUTPUT, OUTPUT, OUTPUT };
+const byte qtdePinosES = 4;
+const byte pinosES[qtdePinosES]   =	{ 0     , 2     , 4 	      , 5            };
+const byte modoPinos[qtdePinosES] =	{ OUTPUT, OUTPUT, INPUT_PULLUP, INPUT_PULLUP };
 
 void setup(){
   
@@ -52,18 +52,20 @@ void loop(){
 					client.println("Content-Type: text/html");
 					client.println();
                 
+					// 
 					if(params.indexOf("digital=") != -1){
 						byte posicaoPassagemParam = params.indexOf("=") + 1;
 						byte pino = params.substring(posicaoPassagemParam, posicaoPassagemParam + 1).toInt();
 						digitalWrite(pino, HIGH);
 						delay(500);
 						digitalWrite(pino, LOW);
+						delay(10);
 					}
 					
 					client.println("<!DOCTYPE html>");
 					client.println("<html>");
 					client.println("<head>");
-         client.println("<meta charset='UTF-8'>");
+					client.println("<meta charset='UTF-8'>");
          
 					client.println("<!--Import Google Icon Font-->");
 					client.println("<link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet'>");
@@ -83,25 +85,25 @@ void loop(){
 					client.println("</div>");
 					client.println("<div class='center-align'>");
 					client.println("<!-- Quarto lado cozinha -->");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 8px;margin-top: 9px;'><i class='material-icons'>wb_sunny</i></a>");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 9px;'><i class='material-icons'>power</i></a>");
+					client.println("<a href='?digital=0' class='btn-floating" + (digitalRead(4) == HIGH) ? " pulse blue-grey " : " " + "lighten-2' style='position: absolute;margin-left: 8px;margin-top: 9px;'><i class='material-icons'>wb_sunny</i></a>");					
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 9px;'><i class='material-icons'>power</i></a>");
 
 					client.println("<!-- Cozinha -->");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 200px;margin-top: 9px;'><i class='material-icons'>wb_sunny</i></a>");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 244px;margin-top: 9px;'><i class='material-icons'>power</i></a>");
+					client.println("<a  href='?digital=2' class='btn-floating" + (digitalRead(5) == HIGH) ? " pulse blue-grey " : " " + "lighten-2' style='position: absolute;margin-left: 200px;margin-top: 9px;'><i class='material-icons'>wb_sunny</i></a>");
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 244px;margin-top: 9px;'><i class='material-icons'>power</i></a>");
 
 					client.println("<!-- Quarto meio -->");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 8px;margin-top: 208px;'><i class='material-icons'>wb_sunny</i></a>");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 208px;'><i class='material-icons'>power</i></a>");
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 8px;margin-top: 208px;'><i class='material-icons'>wb_sunny</i></a>");
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 208px;'><i class='material-icons'>power</i></a>");
 
 					client.println("<!-- Quarto lado sala -->");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 8px;margin-top: 480px;'><i class='material-icons'>wb_sunny</i></a>");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 480px;'><i class='material-icons'>power</i></a>");
+					client.println("<a class='btn-floating  blue-grey lighten-2' style='position: absolute;margin-left: 8px;margin-top: 480px;'><i class='material-icons'>wb_sunny</i></a>");
+					client.println("<a class='btn-floating  blue-grey lighten-2' style='position: absolute;margin-left: 52px;margin-top: 480px;'><i class='material-icons'>power</i></a>");
 
 					client.println("<!-- Sala -->");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 200px;margin-top: 480px;'><i class='material-icons'>wb_sunny</i></a>");
-					client.println("<a class='btn-floating pulse blue-grey lighten-2' style='position: absolute;margin-left: 244px;margin-top: 480px;'><i class='material-icons'>power</i></a>");
-					client.println("");
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 200px;margin-top: 480px;'><i class='material-icons'>wb_sunny</i></a>");
+					client.println("<a class='btn-floating blue-grey lighten-2' style='position: absolute;margin-left: 244px;margin-top: 480px;'><i class='material-icons'>power</i></a>");
+					
 					client.println("<img class='responsive-img' src='https://raw.githubusercontent.com/Rodo1foBandeira/TCC_Logatti/master/Layout/img/Planta.jpg'>");
 
 					client.println("</div>");
@@ -111,7 +113,7 @@ void loop(){
 					client.println("<!--JavaScript at end of body for optimized loading-->");
 					client.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'></script>");
 					client.println("</body>");
-					client.println("</html");
+					client.println("</html>");
 
 					
 					HTTP_req = "";    
